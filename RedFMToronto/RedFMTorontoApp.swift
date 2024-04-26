982e20036e1273fdde887035438c141e440d79f3
Mohit Arora <me@aboutmohit.com>
2024-04-26 09:58:58 -0400

    Initial Commit

---
+++ RedFMToronto/RedFMTorontoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedFMTorontoApp.swift
+//  RedFMToronto
+//
+//  Created by Mohit Arora on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct RedFMTorontoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
